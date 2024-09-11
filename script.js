b018953074c862c6305a29bf2c3421215870138f
farhana shirin  kt <farhanashirinkt96@gmail.com>
2024-09-11 11:33:29 +0530

    gsap

---
+++ script.js
@@ -0,0 +1,244 @@
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.logo div', {
+    opacity: 0,
+    delay: 1,
+    x: 20
+});
+
+const menu_items = document.querySelector('.menu-items');
+gsap.from(menu_items.children, {
+    opacity: 0,
+    x: 0,
+    duration: 1,
+    delay: 1.5,
+    stagger: {
+        amount: 1
+    }
+});
+
+gsap.utils.toArray('.star').forEach(star => {
+    gsap.fromTo(star, {
+        rotation: 450,
+        opacity: 0,
+        y: 100
+    }, {
+        rotation: 0,
+        opacity: 1,
+        y: 0,
+        duration: 1,
+        delay: 1,
+        scrollTrigger: {
+            trigger: star,
+            start: "top 100%",
+            toggleActions: "play none none none"
+        }
+    });
+});
+
+gsap.utils.toArray('.title').forEach(title => {
+    gsap.fromTo(title, {
+        letterSpacing: '10px',
+        opacity: 0,
+        x: 300,
+        skewX: 65
+    }, {
+        letterSpacing: '0',
+        opacity: 1,
+        x: 0,
+        skewX: 0,
+        duration: 1,
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: title
+        }
+    });
+});
+
+gsap.utils.toArray('p').forEach(p => {
+    gsap.fromTo(p, {
+        opacity: 0,
+        x: 150,
+        skewX: 30
+    }, {
+        opacity: 1,
+        x: 0,
+        skewX: 0,
+        duration: 1,
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: p
+        }
+    });
+});
+
+gsap.utils.toArray('button').forEach(button => {
+    gsap.fromTo(button, {
+        opacity: 0
+    }, {
+        opacity: 1,
+        duration: 1,
+        delay: 1,
+        scrollTrigger: {
+            trigger: button
+        }
+    });
+});
+
+gsap.utils.toArray('.img1').forEach(img1 => {
+    gsap.fromTo(img1, {
+        letterSpacing: '10px',
+        opacity: 0,
+        x: 300,
+        skewX: 65
+    }, {
+        letterSpacing: '0',
+        opacity: 1,
+        x: 0,
+        skewX: 0,
+        duration: 1,
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: img1
+        }
+    });
+});
+
+gsap.utils.toArray('.img3').forEach(img3 => {
+    gsap.fromTo(img3, {
+        letterSpacing: '10px',
+        opacity: 0,
+        x: 300,
+        skewX: 65
+    }, {
+        letterSpacing: '0',
+        opacity: 1,
+        x: 0,
+        skewX: 0,
+        duration: 1,
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: img3
+        }
+    });
+});
+gsap.utils.toArray('.lamborghini').forEach(lamborghini => {
+    gsap.fromTo(lamborghini, {
+        letterSpacing: '10px',
+        opacity: 0,
+        x: 300,
+        skewX: 65
+    }, {
+        letterSpacing: '0',
+        opacity: 1,
+        x: 0,
+        skewX: 0,
+        duration: 1,
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: lamborghini
+        }
+    });
+});
+
+
+gsap.to(".lamborghini", {
+    rotate: 360,            // Rotate the car by 360 degrees
+    duration: 2,            // Time for one full rotation
+    repeat: -1,             // Repeat infinitely
+    repeatDelay: 1,         // Delay between repeats (1 second)
+    ease: "linear"          // Use linear easing for a constant speed
+});
+
+
+gsap.utils.toArray('.card').forEach(card => {
+    gsap.fromTo(card, {
+        letterSpacing: '10px',
+        opacity: 0,
+        x: 300,
+        skewX: 65
+    }, {
+        letterSpacing: '0',
+        opacity: 1,
+        x: 0,
+        skewX: 0,
+        duration: 1,
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: card
+        }
+    });
+});
+
+
+
+
+gsap.utils.toArray('a').forEach(a => {
+    gsap.fromTo(a, {
+        letterSpacing: '10px',
+        opacity: 0,
+        x: 300,
+        skewX: 65
+    }, {
+        letterSpacing: '0',
+        opacity: 1,
+        x: 0,
+        skewX: 0,
+        duration: 1,
+        delay: 0.5,
+        scrollTrigger: {
+            trigger: a
+        }
+    });
+});
+
+
+
+
+
+
+// JAVASCIRPT
+// const carPath = {
+//     curviness: 1,
+//     autoRotate: true,
+//     values: [
+//         { x: 0, y: 0 },
+//         { x: window.innerWidth, y: 0 }
+//     ]
+// }
+
+
+// const tween = new TimelineLite();
+// tween.add(
+//     TweenLite.to('.lamborghini', 1, {
+//         bezier: carPath,
+//         ease: Power1.easeInOut
+//     })
+// )
+
+
+// const controller = new ScrollMagic.Controller();
+// const scene = new ScrollMagic.Scene({
+//     triggerElement: ".lambor",
+//     duration: 5000,
+//     triggerHook: 0
+// })
+// .setTween(tween)
+// .setPin('.lambor')
+// .addTo(controller);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
